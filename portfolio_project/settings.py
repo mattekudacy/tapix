0cee0a9fe4b71eff3c7a0bbfba71ec15b6971f4f
Cyrus Mante <mattekudacy>
2025-05-19 01:20:27 +0800

    mysql migrations

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -84,8 +84,15 @@ WSGI_APPLICATION = 'portfolio_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tapix',
+        'USER': 'tapixuser',
+        'PASSWORD': 'yourpassword',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
